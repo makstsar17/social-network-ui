86f02af0f6de56d5e6fec095c22a67a9d3e865e1
makstsar17 <makstsar1701@gmail.com>
2025-02-27 14:07:32 +0200

    feat: set up React, Redux Toolkit, HeroUI

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Card, CardBody } from "@heroui/react"
+
+const App = () => {
+  return (
+    <div>
+      <Card className="max-w-[500px]">
+        <CardBody>
+          <p>Make beautiful websites regardless of your design experience.</p>
+        </CardBody>
+      </Card>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const {heroui} = require("@heroui/react");
+
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [heroui()],
+}
+
+export default config;
